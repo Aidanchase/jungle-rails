e59fd03168e97f8dc3b46390a7ba34f3b19918aa
Aidanchase <aidanmro@gmail.com>
2019-07-11 16:38:02 +0000

    added user log in and registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   
   def create
@@ -15,3 +16,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190711134531) do
+ActiveRecord::Schema.define(version: 20190711154659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
