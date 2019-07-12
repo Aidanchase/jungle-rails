1998360368b33bd59009a0975824f75ab99d8fff
Aidanchase <aidanmro@gmail.com>
2019-07-12 13:53:14 +0000

    fixed naming issues

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,7 @@ class OrdersController < ApplicationController
  
     if order.valid?
       empty_cart!
-      UserMailer.jungle_email(order, current_user).deliver
+      UserMailer.confirmation_mailer(order, current_user).deliver
       redirect_to(order, notice: "order was successfully placed.")
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
